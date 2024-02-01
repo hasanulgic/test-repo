770c19b32e95fef50b152e18444c5e2e5a069c2f
saykot hossain <saykothossain14@gmail.com>
2024-02-01 15:03:20 +0600

    first git commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Test from "./component/test"
+
+function App() {
+
+  return (
+    <>
+      <h1>A test project initiated</h1>
+      <Test/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Test.jsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>This is for testing purpose</h1>
+      <p>This is p tag</p>
+    </div>
+  );
+}
